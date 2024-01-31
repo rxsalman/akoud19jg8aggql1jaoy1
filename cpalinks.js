7daa4805682076d5e9c76d585c9b1473e1f03dda
rxsalman <157442083+rxsalman@users.noreply.github.com>
2024-01-31 17:23:46 +0600

    Update cpalinks.js

--- cpalinks.js
+++ cpalinks.js
@@ -1,4 +1,4 @@
-var gpl30 = "https://vobeluxa.com/cl/0dd58f11289c2de0?p1=&p2=&source=instagram&site=11060";
+var gpl30 = "https://pluxeono.com/cl/9dce85ab84bb7d38?p1=&p2=&source=instagram&site=14163";
 var bll30 = "https://pluxeono.com/cl/42dd17d2f37c67a1?p1=&p2=&source=facebook&site=15075";
 var robil30 = "https://vobeluxa.com/cl/a264878e7a3984c2?p1=&p2=&source=instagram&site=11896";
 var airtel30 = "https://vobeluxa.com/cl/3ef91743f1e55c8c?p1=&p2=&source=instagram&site=13092";
